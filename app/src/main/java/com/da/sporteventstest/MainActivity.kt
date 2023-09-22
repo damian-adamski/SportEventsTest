51e73cfc57bb5100d8198dcaa3a879947cad62f7
wiggy90210 <wiggy90210@gmail.com>
2023-09-22 17:24:39 +0200

    init

---
+++ app/src/main/java/com/da/sporteventstest/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.da.sporteventstest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.da.sporteventstest.ui.theme.SportEventsTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SportEventsTestTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
